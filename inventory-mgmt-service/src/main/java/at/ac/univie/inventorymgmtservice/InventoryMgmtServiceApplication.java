d6980af4eaa58d9554b2f20ba5767f6007701ad1
Johannes Christopher Janka <johannes.janka@hotmail.com>
2024-11-12 15:55:33 +0100

    added inventory microservice

---
+++ inventory-mgmt-service/src/main/java/at/ac/univie/inventorymgmtservice/InventoryMgmtServiceApplication.java
@@ -0,0 +1,13 @@
+package at.ac.univie.inventorymgmtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMgmtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMgmtServiceApplication.class, args);
+	}
+
+}
---
+++ inventory-mgmt-service/src/test/java/at/ac/univie/inventorymgmtservice/InventoryMgmtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.univie.inventorymgmtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMgmtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
