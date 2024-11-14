c37f64704f895dd79d3709105e6b8b7ee5baab98
Johannes Christopher Janka <johannes.janka@hotmail.com>
2024-11-14 16:02:26 +0100

    added inventory opt service

---
+++ inventory-opt-service/src/main/java/at/ac/univie/inventoryoptservice/InventoryOptServiceApplication.java
@@ -0,0 +1,13 @@
+package at.ac.univie.inventoryoptservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryOptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryOptServiceApplication.class, args);
+	}
+
+}
---
+++ inventory-opt-service/src/test/java/at/ac/univie/inventoryoptservice/InventoryOptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.univie.inventoryoptservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryOptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
