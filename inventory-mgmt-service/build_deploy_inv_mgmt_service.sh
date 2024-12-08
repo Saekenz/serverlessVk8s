c3ed19b363fdfa485c71ff2701f78e8ea0f06ea1
Johannes Christopher Janka <johannes.janka@hotmail.com>
2024-12-08 22:26:51 +0100

    added deployment config for inv mgmt service

---
+++ inventory-mgmt-service/build_deploy_inv_mgmt_service.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Define variables
+IMAGE_NAME="inventory-mgmt-service"
+GCR_IMAGE="gcr.io/serverless-k8s-study/$IMAGE_NAME"
+REGION="europe-west4"
+PLATFORM="managed"
+
+# Build and tag the Docker image
+echo "Building Docker image..."
+docker build -t $GCR_IMAGE .
+
+# Push the image to GCR
+echo "Pushing Docker image to Google Container Registry..."
+docker push $GCR_IMAGE
+
+# Deploy the service to Google Cloud Run
+echo "Deploying $IMAGE_NAME to Google Cloud Run..."
+gcloud run deploy $IMAGE_NAME \
+    --image $GCR_IMAGE \
+    --platform $PLATFORM \
+    --region $REGION \
+    --allow-unauthenticated \
+    --set-env-vars MESSAGE_THRESHOLD=10,FIXED_RATE=5000
+# ADD DB CONNECTION VARS
+echo "Deployment completed successfully."
